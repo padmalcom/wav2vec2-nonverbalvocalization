2cbfb12b347b42ba944e16be9b8d217bcee03484
Jonas F <jonas.freiknecht@bwi.de>
2023-02-01 13:03:18 +0100

    Updated the preprocessor to use my emotion model.

--- preprocess_cv.py
+++ preprocess_cv.py
@@ -21,8 +21,8 @@ irony_model = AutoModelForSequenceClassification.from_pretrained(IRONY_MODEL_NAM
 
 def irony(text):
 	labels = ['no irony', 'irony']
-	encoded_input = tokenizer(text, return_tensors='pt')
-	output = model(**encoded_input)
+	encoded_input = irony_tokenizer(text, return_tensors='pt')
+	output = irony_model(**encoded_input)
 	scores = output[0][0].detach().numpy()
 	scores = softmax(scores)
 	ranking = np.argsort(scores)
@@ -38,11 +38,23 @@ def irony(text):
 	return max_label
 	
 ### emotion
-EMOTION_MODEL_NAME = "ehcalabres/wav2vec2-lg-xlsr-en-speech-emotion-recognition"
-audio_classifier = pipeline(task="audio-classification", model="ehcalabres/wav2vec2-lg-xlsr-en-speech-emotion-recognition")
-emotions = ['angry', 'calm', 'disgust', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
+EMOTION_MODEL_NAME = "padmalcom/wav2vec2-large-emotion-detection-german"
+emotions = {'anger':0, 'boredom':1, 'disgust':2, 'fear':3, 'happiness':4, 'sadness':5, 'neutral':6}
+audio_classifier = pipeline(task="audio-classification", model=EMOTION_MODEL_NAME)
 
-
+def emotion(audio_file):
+	preds = audio_classifier(audio_file)
+	max_score = 0
+	max_label = 6
+	max_label_text = ""
+	for p in preds:
+		if p['score'] > max_score and p['score'] > 0.3:
+			max_score = p['score']
+			max_label = emotions[p["label"]]
+			max_label_text = p["label"]
+			print("There is an emotional file:", max_label_text)
+	return max_label
+	
 ### translation
 TRANSLATION_MODEL_NAME = "facebook/wmt19-de-en"
 translation_tokenizer = FSMTTokenizer.from_pretrained(TRANSLATION_MODEL_NAME)
@@ -77,7 +89,7 @@ def prepare_data():
 			formatted_sample['age'] = line['age']
 			formatted_sample['gender'] = line['gender']
 			formatted_sample['language'] = line['locale']
-			formatted_sample['file'] = line['path']
+			
 			# '0' non irony, '1' irony
 			formatted_sample['irony'] = irony(translate(formatted_sample['sentence']))
 			
@@ -89,23 +101,16 @@ def prepare_data():
 					sound = sound.set_channels(1)
 					wav_path = os.path.join('common-voice-12', "wavs", filename + ".wav")
 					sound.export(wav_path, format="wav")
+					formatted_sample['file'] = filename + ".wav"
 					
 					# emotion classification
-					preds = audio_classifier(wav_path)
-					for p in preds:
-						print(p)
-					#return [{"score": round(pred["score"], 4), "label": pred["label"]} for pred in preds]
-					formatted_sample['emotion'] = p["label"]
-					# 'neu' => 0, 'ang' => 1, 'hap' => 2, 'sad' => 3
-					
-					# 
+					formatted_sample['emotion'] = emotion(wav_path)
+
 					data.append(formatted_sample)
 					i += 1
-			if i == 100:
-				break
 			
 		random.shuffle(data)
-		print("Found", len(data), "samples. Example: ", data[:5])
+		print("Found", len(data), "samples. Example: ", data[:1])
 		
 		train = data[:int(len(data)*0.8)]
 		test = data[len(train):]
